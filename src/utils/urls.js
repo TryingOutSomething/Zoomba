1c192ac82818fde61578a856cb8518b8ad4c468d
TryingOutSomething <jan.owyeong123@gmail.com>
2020-10-18 15:11:45 +0800

    Test

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -18,3 +18,4 @@ export const loginRoute = `${_authPaths.MAIN}`
 export const dashboardUserRoute = `${_dashboardPagePaths.MAIN}/${_dashboardPagePaths.USER}`
 export const dashboardRankingRoute = `${_dashboardPagePaths.MAIN}/${_dashboardPagePaths.RANKINGS}`
 export const dashboardMainPageRoute = `${_dashboardPagePaths.MAIN}/${_dashboardPagePaths.USER}`
+
