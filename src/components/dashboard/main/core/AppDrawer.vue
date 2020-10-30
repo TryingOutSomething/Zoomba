b6d19002fb9cb85f57d1a925c588426f9c1691a7
TryingOutSomething <jan.owyeong123@gmail.com>
2020-10-30 18:30:11 +0800

    Change rankings to home for app drawer in dashboard

--- src/components/dashboard/main/core/AppDrawer.vue
+++ src/components/dashboard/main/core/AppDrawer.vue
@@ -54,7 +54,7 @@ export default {
         {
           to: dashboardRankingRoute,
           icon: 'mdi-podium',
-          text: 'Rankings',
+          text: 'Home',
           name: 'View Rankings'
         },
         {
