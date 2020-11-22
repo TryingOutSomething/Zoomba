3a9af284af68e3e3462af55c96c92027fd45c598
TryingOutSomething <jan.owyeong123@gmail.com>
2020-11-22 14:56:24 +0800

    Really removed all firebase dependencies

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,26 +1,26 @@
-import Vue from 'vue'
-import { firestorePlugin } from 'vuefire'
+// import Vue from 'vue'
+// import { firestorePlugin } from 'vuefire'
 
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+// import 'firebase/auth'
+//
+// const config = {
+//   apiKey: '',
+//   authDomain: '',
+//   databaseURL: '',
+//   projectId: '',
+//   storageBucket: '',
+//   messagingSenderId: '',
+//   appId: '',
+//   measurementId: ''
+// }
 
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
-}
+// const app = firebase.initializeApp(config)
+//
+// export const authenticator = app.auth()
+//
+// export const firestore = app.firestore()
+// export const { Timestamp } = firebase.firestore
 
-const app = firebase.initializeApp(config)
-
-export const authenticator = app.auth()
-
-export const firestore = app.firestore()
-export const { Timestamp } = firebase.firestore
-
-Vue.use(firestorePlugin)
+// Vue.use(firestorePlugin)
