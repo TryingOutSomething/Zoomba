49bd0a8e25fd7f4c425f760002c391e9941a6df7
TryingOutSomething <jan.owyeong123@gmail.com>
2020-11-22 21:53:37 +0800

    Disable source map option for build

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,6 @@ module.exports = {
     disableHostCheck: true
   },
 
-  publicPath: process.env.NODE_ENV === 'production' ? '/Zoomba' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Zoomba' : '/',
+  productionSourceMap: false
 }
